bceb1198c71826939fe07e19eb0bef69af558a5f
Valery Emelyanov <emelva@rambler.ru>
2020-03-19 21:33:05 +0300

    JUnit tests for LoginController

---
+++ src/test/java/com/example/notes/controllers/LoginControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.notes.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class LoginControllerTest {
+
+    @InjectMocks
+    private LoginController loginController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/templates/");
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(loginController)
+                .setViewResolvers(viewResolver)
+                .build();
+    }
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+
+    }
+}
